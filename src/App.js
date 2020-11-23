6c5e1148a4a2af2deb00fdae2143361d894af74d
Iliyan-Y <ili.yankov91@gmail.com>
2020-11-23 18:37:32 +0000

    Sceleton interface

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Container, Nav, Jumbotron, Image } from 'react-bootstrap';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import Logo01 from './assets/Logo01.png';
+import { GrPhone } from 'react-icons/gr';
 
 function App() {
   return (
-    <Container className="App">
+    <Container className="mw-100 p-0">
       <Navbar sticky="top" bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <AnchorLink className="nav-link" href="#home">
+              Home
+            </AnchorLink>
+            <AnchorLink className="nav-link" href="#about">
+              About
+            </AnchorLink>
           </Nav>
         </Navbar.Collapse>
+        <AnchorLink href="#home" style={{ marginRight: '30%' }}>
+          <Image src={Logo01} />
+        </AnchorLink>
+        <h3>
+          <GrPhone /> Tel:0000000000
+        </h3>
       </Navbar>
-      <Container className="h-100 align-items-center justify-content-center text-center">
-        <h1>Your Favorite Source of Free Bootstrap Themes</h1>
-      </Container>
+
+      <Jumbotron id="home" fluid style={{ height: '100vh' }} className="m-0">
+        <div className="row h-100 align-items-center justify-content-center text-center">
+          <h1> Carpenters & BUILDERS BASED IN London </h1>
+        </div>
+      </Jumbotron>
+
+      <Jumbotron fluid style={{ height: '100vh' }} className="m-0">
+        <div className="row h-100 align-items-center justify-content-center text-center">
+          <h1> Contact: </h1>
+        </div>
+      </Jumbotron>
+
+      <Jumbotron id="about" fluid style={{ height: '100vh' }} className="m-0">
+        <div className="row h-100 align-items-center justify-content-center text-center">
+          <h1> About US: </h1>
+        </div>
+      </Jumbotron>
     </Container>
   );
 }
